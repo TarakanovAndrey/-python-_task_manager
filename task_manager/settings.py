a5f974fa7a4a97f48bfe6e0aef90ae3eaf4f5085
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-10-23 19:10:04 +0300

    Add .env

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -118,13 +118,13 @@ DATABASES = {
     # ImproperlyConfigured exception if not found
     #
     # The db() method is an alias for db_url().
-    'default': env.db_url(
-        'SQLITE_URL',
-        default='sqlite:////tmp/my-tmp-sqlite.db'
-    ),
+    'default': env.db(),
 
     # read os.environ['SQLITE_URL']
-    'extra': env.db()
+    'extra': env.db_url(
+        'SQLITE_URL',
+        default='sqlite:////tmp/my-tmp-sqlite.db'
+    )
 }
 
 
