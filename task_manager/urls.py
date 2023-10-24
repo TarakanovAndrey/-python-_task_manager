acfa0eb3d220486174fd997b026878a582039996
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-10-24 11:01:07 +0300

    Add .env

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -4,14 +4,24 @@ from django.urls import path, include
 from . import views
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-] + i18n_patterns(
-    path('i18n/', include('django.conf.urls.i18n')),
-    path('', views.get_home_page, name='home'),
-    path('', include('users.urls')),
-    path('', include('statuses.urls')),
-    path('', include('labels.urls')),
-    path('', include('tasks.urls')),
-    prefix_default_language=False
-)
+urlpatterns = i18n_patterns(path('admin/', admin.site.urls),
+                            # path('i18n/', include('django.conf.urls.i18n')),
+                            path('', views.get_home_page, name='home'),
+                            path('', include('users.urls')),
+                            path('', include('statuses.urls')),
+                            path('', include('labels.urls')),
+                            path('', include('tasks.urls')),
+                            prefix_default_language=False,
+                        )
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ] + i18n_patterns(
+#     path('i18n/', include('django.conf.urls.i18n')),
+#     path('', views.get_home_page, name='home'),
+#     path('', include('users.urls')),
+#     path('', include('statuses.urls')),
+#     path('', include('labels.urls')),
+#     path('', include('tasks.urls')),
+#     prefix_default_language=False
+# )
