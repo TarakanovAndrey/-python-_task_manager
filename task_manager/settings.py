9f617674c0d651e1a65e29d631a8e5305345eca2
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-10-24 13:02:02 +0300

    Check rollbar

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -75,7 +75,17 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
+
+
+ROLLBAR = {
+    'access_token': '14d5bc0a419044a29fbef722554eed5a',
+    'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
+    'root': BASE_DIR,
+}
+
 
 ROOT_URLCONF = 'task_manager.urls'
 
--- task_manager/views.py
+++ task_manager/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def get_home_page(request):
     return render(
         request,
-        template_name='task_manager/index.html',
+        template_name='task_manager/index.htmldd',
     )
