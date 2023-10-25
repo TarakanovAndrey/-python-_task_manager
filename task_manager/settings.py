df003be94dbb3f391cb749e24ac005a5c2dc55ad
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-10-25 20:36:38 +0300

    Refactoring

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -119,31 +119,13 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-<<<<<<< HEAD
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-=======
-DATABASES = {
-    'default': env.db(),
-    'extra': env.db_url(
-        'SQLITE_URL',
-        default='sqlite:////tmp/my-tmp-sqlite.db'
->>>>>>> f12a4ef (Refactoring)
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
-
 
 
 # Password validation
