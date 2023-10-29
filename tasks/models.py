b0074d1fc93312474fdb559687587e10eb8f6929
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-10-29 22:01:22 +0300

    update task create

--- tasks/models.py
+++ tasks/models.py
@@ -11,7 +11,12 @@ class Task(models.Model):
     author = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True, related_name='author')
     executor = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True,)
     status = models.ForeignKey(Status, on_delete=models.PROTECT, blank=True, null=True)
-    labels = models.ManyToManyField(Label, blank=True)
+    labels = models.ManyToManyField(Label, blank=True, related_name='labels', through='TaskLabel')
 
     def __str__(self):
         return self.name
+
+
+class TaskLabel(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    label = models.ForeignKey(Label, on_delete=models.RESTRICT)
