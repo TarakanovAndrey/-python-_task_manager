3879b2e6a9028a40965c6fb415b98153a064ddd4
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-10-30 20:00:02 +0300

    add tests in tasks/tests.py

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,25 @@
-# from django.test import TestCase
+from django.test import TestCase
+from . models import Task
+from django.contrib.auth.models import User
+from statuses.models import Status
 
-# Create your tests here.
+
+class TaskTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='user1', password='user1')
+        self.status = Status.objects.create(name='status1')
+        Task.objects.create(name="Task1", status=self.status, author=self.user)
+
+    def test_task_create(self):
+        task = Task.objects.get(name='Task1')
+        self.assertEqual(task.name, 'Task1')
+
+    def test_label_update(self):
+        task = Task.objects.get(name='Task1')
+        task.name = 'Task2'
+        self.assertEqual(task.name, 'Task2')
+
+    def test_label_delete(self):
+        task = Task.objects.get(name='Task1')
+        task.delete()
+        self.assertNotIn(task, Task.objects.all())
