1efcb8bdfa8c2d250323481f58370576ae4bcf52
Andrey Tarakanov <andrey.t-krd@yandex.ru>
2023-11-01 19:29:42 +0300

    Update users/tests.py

--- users/tests.py
+++ users/tests.py
@@ -17,13 +17,13 @@ class UserTestCase(TestCase):
         self.assertEqual(user.first_name, 'User')
 
     def test_user_login(self):
-        c = Client()
-        response = c.post('/login/', {'username': 'User', 'password': 111})
+        client = Client()
+        response = client.post('/login/', {'username': 'User', 'password': 111})
         self.assertEqual(response.status_code, 200)
 
     def test_user_logout(self):
-        c = Client()
-        response = c.post('/logout/', {'username': 'User', 'password': 111})
+        client = Client()
+        response = client.post('/logout/', {'username': 'User', 'password': 111})
         self.assertEqual(response.status_code, 302)
 
     def test_user_update(self):
